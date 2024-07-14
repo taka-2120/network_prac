d67e0c54a4324cb641029594169149751c857e86
taka-2120 <yu_tkhs@icloud.com>
2024-07-14 23:29:47 +0900

    add: options

---
+++ 08/b8-3/b8-3.py
@@ -0,0 +1,66 @@
+from cgi import FieldStorage
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+with open("b8-3in.html", mode="r", encoding="utf-8") as file:
+    input = file.read()
+with open("b8-3out.html", mode="r", encoding="utf-8") as file:
+    output = file.read()
+
+
+class MyServerHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        html = input.format(title="イベント検索", message="")
+        self.wfile.write(html.encode("utf-8"))
+        return
+
+    def do_POST(self):
+        form = FieldStorage(
+            fp=self.rfile, headers=self.headers, environ={"REQUEST_METHOD": "POST"}
+        )
+        event_id = form["event_id"].value
+
+        if event_id == "1":
+            message = """
+            <tr>
+                <td>1</td>
+                <td>予定1</td>
+                <td>2024/04/01</td>
+                <td>場所1</td>
+            </tr>
+            """
+        elif event_id == "2":
+            message = """
+            <tr>
+                <td>2</td>
+                <td>予定2</td>
+                <td>2024/04/02</td>
+                <td>場所2</td>
+            </tr>
+            """
+        elif event_id == "3":
+            message = """
+            <tr style=\"color:chocolate\">
+                <td>3</td>
+                <td>予定3</td>
+                <td>2024/09/30</td>
+                <td>場所3</td>
+            </tr>
+            """
+        else:
+            message = "<p>イベントIDが正しくありません。</p>"
+
+        self.send_response(200)
+        self.end_headers()
+        html = output.format(title="イベント詳細", message=message)
+        self.wfile.write(html.encode("utf-8"))
+        return
+
+    def error(self):
+        self.send_error(404, "FILE NOT FOUND")
+
+        return
+
+
+HTTPServer(("", 8000), MyServerHandler).serve_forever()
